fe67eced52879b7612302d47609de379d84a3905
Luban <57076960+Luban6887@users.noreply.github.com>
2022-08-10 02:13:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import ws
 
-lbn = []
-lx = 0
+lst = []
+count = 0
 string = ("my name is luban. i am 22 years old. i am a student of electrical engineering")
 
 
-ws.SepWords(string,lbn)
+ws.SepWords(string,lst)
 
 for something in lbn:
     print(lbn[lx])
-    lx+=1
+    count+=1
